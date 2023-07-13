c2b558026133c01da28d7ed8c649c8bebc4b3f2c
Yetunde Alese <yaalese1@gmail.com>
2023-07-13 13:54:59 -0400

    implemented inter font typeface, personal image

--- yetunde_alese/src/about.ts
+++ yetunde_alese/src/about.ts
@@ -1,11 +1,6 @@
-export function getAboutMe(){
-    return (
-     
-        "Hi, My Name is (Yeh-TUW-NDey) some also call me Yaya!                 I'm an experienced Full Stack Developer with a strong focus on JavaScript, React.js, and Ruby on Rails. I have Healthcare, Customer service and Analytical experience that allows me to design innovative and well-structured projects or debug and improve already designed software architectures. I have designed and collabed on many projects highlighting my skills and knowledge. I am always eager to learn more and improve my skills so that I can provide the best possible solutions to my clients or team. If you are in need of any help with your Full Stack Development project, I am here to assist you. Let's work together to achieve your goals!"
+export function getAboutMe(element: HTMLButtonElement){
     
-  
-    
-    );
+    element.addEventListener("click", () => getAboutMe)
     
         
 
