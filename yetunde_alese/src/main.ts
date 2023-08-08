0b9bd0b8c0d6a20913379bf19796f388bc1aa03c
Yetunde Alese <yaalese1@gmail.com>
2023-08-08 05:09:01 -0400

    header and animation added need to fix spacing and animation size

--- yetunde_alese/src/main.ts
+++ yetunde_alese/src/main.ts
@@ -7,7 +7,7 @@ import './style.css'
 
 // document.querySelector<HTMLDivElement>('#app')!.innerHTML = 
 
-var mainListDiv = document.getElementById("mainListDiv"),
+const mainListDiv = document.getElementById("mainListDiv"),
     mediaButton = document.getElementById("mediaButton");
     if (mediaButton != null) //used if statement that serves as a type guard fix error , this chest to see if media button is null and undefined
         mediaButton.onclick = function () {
@@ -17,21 +17,23 @@ var mainListDiv = document.getElementById("mainListDiv"),
 // used using optional chaining to get rid of obj possibly null error     
         }
 
+const links = document.querySelectorAll(".page-header ul a");
 
-        const links = document.querySelectorAll(".page-header ul a");
-        for (const link of links) {
-          link.addEventListener("click", clickHandler);
-        }
-        function clickHandler(e) {
-          e.preventDefault();
-          const href = this.getAttribute("href");
-          console.log(href);
-          const offsetTop = document.querySelector(href).offsetTop;
-          scroll({
-            top: offsetTop,
-            behavior: "smooth"
-          });
-        }
+  for (const link of links) {
+  link.addEventListener("click", clickHandler);
+    }
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href: string = this.getAttribute("href");
+
+  const offsetTop = document.querySelector(href)?.offsetTop;
+
+    scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
 
 
 
